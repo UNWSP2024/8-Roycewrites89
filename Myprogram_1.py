03e6687be8180d72fd1112ae03995aa66b2a6741
Roycewrites89 <roycedaniel909@gmail.com>
2026-03-20 15:32:46 -0500

    Add files via upload

---
+++ Myprogram_1.py
@@ -0,0 +1,6 @@
+#Royce Daniel, 3/20/2026, "Say your name"
+first = str(input("Please enter your first name:"))
+second = str(input("Please enter your middle name:"))
+third = str(input("Please enter your last name:"))
+
+print("Your initials are", first[0],".", second[0],".", third[0],".")
